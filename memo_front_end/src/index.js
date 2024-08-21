f9997b7e1404e7a0772da6ae6f09472b1eb16de2
AndrewAscone <andrew.ascone@gmail.com>
2024-08-21 18:28:46 -0400

    Styling front end

--- memo_front_end/src/index.js
+++ memo_front_end/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './styles/main.css'
 
 const App = () =>{
     return(
         <div>
-            <h1>Hello world</h1>
+            <div className="header">
+                <div className="logo">
+                    <p className="title">Memo</p>
+                </div>
+                <div className="add-section">
+                    <a className="add-btn" href='#'>Make a Memo</a>
+                </div>
+            </div>
         </div>
     )
 }
