15ff846a3923095fc1daeeb35daef335348d0a36
AndrewAscone <andrew.ascone@gmail.com>
2024-08-21 19:13:30 -0400

    Adding styling, state hooks

--- memo_front_end/src/index.js
+++ memo_front_end/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './styles/main.css'
 
 const App = () =>{
+
+    const [modalVisible,setModalVisible]=useState(false);
     return(
         <div>
             <div className="header">
@@ -10,26 +12,37 @@ const App = () =>{
                     <p className="title">Memo</p>
                 </div>
                 <div className="add-section">
-                    <a className="add-btn" href='#'>Write a Memo</a>
+                    <a className="add-btn" href='#'
+                        onClick={()=>setModalVisible(true)}
+                    >Write a Memo</a>
                 </div>
             </div>
             <div className="memos">
                 <p className="centerText">No Memos</p>
             </div>
-            <div className="modal">
+            <div className={modalVisible? 'modal' : 'modal-not-visible'}>
                 <div className="form">
                     <div className="form-header">
                         <div>
                             <p className="form-header-text">Write a Memo</p>
                         </div>
                         <div>
-                             <a href="#" className='close-btn'>X</a>
+                             <a href="#" className='close-btn'
+                                onClick={()=>setModalVisible(!modalVisible)}
+                             >X</a>
                         </div>
                     </div>
                     <form action="">
                         <div className="form-group">
                             <label htmlFor="title">Title</label>
                             <input type="text" name="title" id="title" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="content">Text</label>
+                            <textarea name="content" id="" cols="30" rows="5" className="form-control"></textarea>
+                        </div>
+                        <div className="form-group">
+                            <input type="submit" value="Save" className='btn'/>
                         </div>
                     </form>
                 </div>
