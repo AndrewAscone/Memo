35d09fcd1db3763e15868d40a95efda8a7a1b48f
AndrewAscone <andrew.ascone@gmail.com>
2024-08-21 17:00:28 -0400

    A whole bunch of backend work and dependency installs

--- memo_api/memo/models.py
+++ memo_api/memo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Memo:
+class Memo(models.Model):
     title=models.CharField(max_length=255)
     content=models.TextField()
     date_created=models.DateTimeField(auto_now_add=True)
--- memo_api/memo/views.py
+++ memo_api/memo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Memo
+from rest_framework import viewsets
+from .serializers import MemoSerializer
 
 # Create your views here.
+
+class MemoViewset(viewsets.ModelViewSet):
+    serializer_class=MemoSerializer
+    queryset=Memo.objects.all()
