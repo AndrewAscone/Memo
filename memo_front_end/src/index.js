de630f80c3330ea854646a93d2d0d0e18ab23b35
AndrewAscone <andrew.ascone@gmail.com>
2024-08-21 18:58:44 -0400

    Making modal window

--- memo_front_end/src/index.js
+++ memo_front_end/src/index.js
@@ -10,7 +10,28 @@ const App = () =>{
                     <p className="title">Memo</p>
                 </div>
                 <div className="add-section">
-                    <a className="add-btn" href='#'>Make a Memo</a>
+                    <a className="add-btn" href='#'>Write a Memo</a>
+                </div>
+            </div>
+            <div className="memos">
+                <p className="centerText">No Memos</p>
+            </div>
+            <div className="modal">
+                <div className="form">
+                    <div className="form-header">
+                        <div>
+                            <p className="form-header-text">Write a Memo</p>
+                        </div>
+                        <div>
+                             <a href="#" className='close-btn'>X</a>
+                        </div>
+                    </div>
+                    <form action="">
+                        <div className="form-group">
+                            <label htmlFor="title">Title</label>
+                            <input type="text" name="title" id="title" className="form-control" />
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
